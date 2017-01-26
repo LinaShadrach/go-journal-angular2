620e2cf04086728de710553c6520bc589ddc0553
Erica Nafziger and Lina Shadrach <lina@>
2017-01-26 10:44:35 -0800

    add ability to update pledge amount

--- src/app/new-backer/new-backer.component.ts
+++ src/app/new-backer/new-backer.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-new-backer',
   templateUrl: './new-backer.component.html',
-  styleUrls: ['./new-backer.component.css']
+  styleUrls: ['./new-backer.component.css'],
+  providers: [ProjectService]
 })
 export class NewBackerComponent implements OnInit {
+  projectId: string;
+  projectToBack;
 
-  constructor() { }
+  constructor(private projectService: ProjectService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.projectId = urlParameters['id'];
+    });
+    this.projectToBack = this.projectService.getProjectById(this.projectId);
+  }
+
+  submitPledge(name:string, pledge: number) {
+    var oldAmount = this.projectService.getProjectBackers(this.projectId, name, pledge);
+
   }
 
 }
--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Project } from './project.model';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class ProjectService {
   projects: FirebaseListObservable<any[]>;
   popularProjects: FirebaseListObservable<any[]>;
-
+  projectBackers: FirebaseListObservable<any[]>;
+  oldAmount;
   constructor(private angularFire: AngularFire) {
     this.projects = angularFire.database.list('projects');
     this.popularProjects = this.angularFire.database.list('/projects/', {query: {orderByChild: "popularity", limitToLast: 1}});
+  }
+
+  getProjectById(projectId: string) {
+    return this.angularFire.database.object('/projects/' + projectId);
+  }
+
+  getProjectBackers(projectId: string, name: string, pledge){
+    this.projectBackers= this.angularFire.database.list('/projects/' + projectId + '/backers');
+    this.projectBackers.push(name);
+    var selectedProject = this.getProjectById(projectId);
+    var updatedPledge = this.angularFire.database.object('/projects/' + projectId + '/pledged/').subscribe(data => {
+      this.oldAmount =data.$value;
+    });
+    this.oldAmount+=parseInt(pledge);
+    selectedProject.update({pledged: this.oldAmount});
   }
 
   getProjects() {
@@ -21,8 +39,6 @@ export class ProjectService {
   getMostPopularProject(){
     return this.popularProjects;
   }
-  getProjectById(projectId: string) {
-    return this.angularFire.database.object('/projects/' + projectId);
-  }
+
 
 }
